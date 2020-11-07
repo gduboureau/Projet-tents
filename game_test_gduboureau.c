7093b578ef6dd14de5800721b8dcb94d7f1d28cd
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2020-11-07 11:25:34 +0100

    game-test-gduboureau.c

--- game_test_gduboureau.c
+++ game_test_gduboureau.c
@@ -1,17 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <game.h>
+#include <game_aux.h>
+
+bool test_game_fill_grass_row(game g, uint i){
+    test_game_fill_grass_row(g,i);
+    square squares[]={};
+    for (unsigned int i; i<(i+1); i++){
+        for (unsigned int j = 0; j<DEFAULT_SIZE;j++){
+            if (game_check_move(g, i, j, GRASS)==ILLEGAL){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 void usage (int argc, char *argv[]){
     fprintf(stderr, "Usage: %s <testname> (trop ou pas assez d'arguments)\n", argv[0]);
 }
 
 int main (int argc, char* argv[]){
-    if (argc == 2){
-        if (strcmp(argv[1], "dummy") == 0){
-            return 0;
-        }
-        return 1;
+    game g=game_default();
+    if (strcmp(argv[1], "test_game_fill_grass_row") == 0){
+        test_game_fill_grass_row(g,argv[1]);
     }
-    usage(argc,argv);
 }
