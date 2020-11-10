d5a262e656bc096a7c4a3b0511f582061af1ef20
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2020-11-10 16:52:36 +0100

    game_test_gduboureau + modif cmakelists

--- game_test_gduboureau.c
+++ game_test_gduboureau.c
@@ -2,15 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include <game.h>
-#include <game_aux.h>
+#include "game.h"
+#include "game_aux.h"
 
-bool test_game_fill_grass_row(game g, uint i){
-    test_game_fill_grass_row(g,i);
-    square squares[]={};
-    for (unsigned int i; i<(i+1); i++){
-        for (unsigned int j = 0; j<DEFAULT_SIZE;j++){
-            if (game_check_move(g, i, j, GRASS)==ILLEGAL){
+
+void usage (int argc, char *argv[]){
+    fprintf(stderr, "Usage: %s <testname> (trop ou pas assez d'arguments)\n", argv[0]);
+}
+
+
+/* *********************************************************** */
+
+bool test_game_fill_grass_row(){
+    game g = game_default();
+    for (unsigned int i; i<DEFAULT_SIZE; i++){
+        for(unsigned int j = 0 ; j<DEFAULT_SIZE; j++){
+            if (game_check_move(g,i,j,TREE) == ILLEGAL){
                 return false;
             }
         }
@@ -18,13 +25,50 @@ bool test_game_fill_grass_row(game g, uint i){
     return true;
 }
 
-void usage (int argc, char *argv[]){
-    fprintf(stderr, "Usage: %s <testname> (trop ou pas assez d'arguments)\n", argv[0]);
+/* *********************************************************** */
+
+bool test_game_restart(){
+    game g = game_default();
+    for (unsigned int i = 0; i<DEFAULT_SIZE; i++){
+        for (unsigned int j = 0; j<DEFAULT_SIZE; j++){
+            if (game_get_square(g,i,j) == TENT || game_get_square(g,i,j) == GRASS){
+                return false;
+            }
+        }
+    }
+    if (game_is_over(g) == true){
+        return false;
+    }
+    return true;
 }
 
+/* *********************************************************** */
+
+bool test_game_print(){
+    game g = game_default();
+    game_print(g);
+    return true;
+}
+
+/* *********************************************************** */
+
 int main (int argc, char* argv[]){
-    game g=game_default();
-    if (strcmp(argv[1], "test_game_fill_grass_row") == 0){
-        test_game_fill_grass_row(g,argv[1]);
+    if (argc == 1){
+        usage(argc, argv);
+    }
+    fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
+    bool ok = false;
+    if (strcmp("game_restart",argv[1]) == 0){
+        ok =test_game_restart();
+    }else{
+        fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    if (ok) {
+        fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
+        return EXIT_SUCCESS;
+    }else{
+        fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
+        return EXIT_FAILURE;
     }
 }
