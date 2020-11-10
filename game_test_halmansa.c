872c436a540485bb887e0ea27be36ca16e32c9ce
Almansa Hugo <hugo.almansa@etu.u-bordeaux.fr>
2020-11-10 21:33:52 +0100

    test hugo v2

--- game_test_halmansa.c
+++ game_test_halmansa.c
@@ -1,133 +1,124 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include "game.h"
-#include "game_aux.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-void usage(int argc, char *argv[]){
-    fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
-    exit(EXIT_FAILURE);
+#include "game.h"
+#include "game_aux.h"
+
+
+
+void usage(int argc, char *argv[]) {
+  fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
+  exit(EXIT_FAILURE);
 }
 
-/* *********************************************************** */
+/* *********************************************************** */
 
-bool test_game_get_square(void){
-    game g = game_default();
-    unsigned int i=0;
-    unsigned int j=0;
-
-    for(i; i<DEFAULT_SIZE; i++){
-        for(j; j<DEFAULT_SIZE; j++){
-            if(game_get_square(g, i, j) != TENT || game_get_square(g, i, j) != TREE || game_get_square(g, i, j) != GRASS || game_get_square(g, i, j) != EMPTY){
-                fprintf(stderr,"Error : the square isn't valid!\n");
-                return false;
-            } 
-        }
-    }
-    return true;
+bool test_game_get_square(void){
+    game g = game_default();
+    unsigned int i = 0;
+    unsigned int j = 0;
+    for(i; i<DEFAULT_SIZE; i++){
+        for(j; j<DEFAULT_SIZE; j++){
+            if(game_get_square(g, i, j) != TENT && game_get_square(g, i, j) != EMPTY && game_get_square(g, i, j) != GRASS && game_get_square(g, i, j) != TREE){
+                fprintf(stderr, "Error : the square isn't valid!\n");
+                return false;
+            }
+        }    
+    }
+    return true;
 }
 
-/* *********************************************************** */
+/* *********************************************************** */
 
-/* bool test_game_set_expected_nb_tents_row(game g, uint i, uint nb_tents){
-    game_set_expected_nb_tents_row(g, i, nb_tents);
+//bool test_game_set_expected_nb_tents_row(void){
+//}
 
-    return true;
-    }
-*/
+/* *********************************************************** */
 
-/* *********************************************************** */
+//bool test_game_set_expected_nb_tents_col(void){
+//}
 
-/* bool test_game_set_expected_nb_tents_col(game g, uint j, uint nb_tents){
-    }
-*/
+/* *********************************************************** */
 
-/* *********************************************************** */
-
-bool test_game_get_expected_nb_tents_row(void){
-    game g = game_default();
-    unsigned int i=0;
-    for(i; i<DEFAULT_SIZE; i++){
-        if(game_get_expected_nb_tents_row(g, i)<0){
-            fprintf(stderr,"Error : number of tents isn't valid!\n");
-            return false;
-        }
-    }
-    return true;
+bool test_game_get_expected_nb_tents_row(void){
+    game g = game_default();
+    unsigned int i = 0;
+    for(i; i<DEFAULT_SIZE; i++){
+        if(game_get_expected_nb_tents_row(g, i)<0){
+            fprintf(stderr, "Error : number of tents isn't valid!\n");
+            return false;
+        }
+    }    
+    return true;
 }
 
-   
-/* *********************************************************** */
+/* *********************************************************** */
 
-bool test_game_get_expected_nb_tents_col(void){
-    game g = game_default();
-    unsigned int j=0;
-    for(j; j<DEFAULT_SIZE; j++){
-        if(game_get_expected_nb_tents_row(g, j)<0){
-            fprintf(stderr,"Error : number of tents isn't valid!\n");
-            return false;
-        }
-    }
-    return true;
+bool test_game_get_expected_nb_tents_col(void){
+    game g = game_default();
+    unsigned int j = 0;
+    for(j; j<DEFAULT_SIZE; j++){
+        if(game_get_expected_nb_tents_row(g, j)<0){
+            fprintf(stderr, "Error : number of tents isn't valid!\n");
+            return false;
+        }
+    }    
+    return true;
 }
 
-/* *********************************************************** */
+/* *********************************************************** */
 
-/* bool test_game_get_current_nb_tents_all(cgame g){
-    }
-*/
-/* *********************************************************** */
+//bool test_game_get_expected_nb_tents_all(void){
+//}
 
-int main(int argc, char *argv[]) {
-  if (argc == 1){
-    usage(argc, argv);
-  }
+/* *********************************************************** */
 
-   // start test
-  fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
+int main(int argc, char *argv[]) {
+  if (argc == 1){
+    usage(argc, argv);
+  }
 
-  bool ok = false;
+   // start test
+  fprintf(stderr, "=> Start test \"%s\"\n", argv[1]);
 
-   if(strcmp("game_get_square", argv[1]) == 0){
-    ok = test_game_get_square();
-  }
-  /*
-  else if(strcmp("game_copy", argv[1]) == 0){
-    ok = test_game_copy();
-  }
-  else if(strcmp("game_equal", argv[1]) == 0){
-    ok = test_game_equal();
-  }
-  */
-  else if(strcmp("game_get_expected_nb_tents_row", argv[1]) == 0){
-    ok = test_game_get_expected_nb_tents_row();
-  }
- 
-  else if(strcmp("game_get_expected_nb_tents_col", argv[1]) == 0){
-    ok = test_game_get_expected_nb_tents_col();
-  }
+  bool ok = false;
 
-     /*
+  if(strcmp("game_get_square", argv[1]) == 0){
+    ok = test_game_get_square();
+  }
+  /*
+  else if(strcmp("game_set_expected_nb_tents_row", argv[1]) == 0){
+    ok = test_game_set_expected_nb_tents_row();
+  }
+  else if(strcmp("game_set_expected_nb_tents_col", argv[1]) == 0){
+    ok = test_game_set_expected_nb_tents_col();
+  }
+  */
+  else if(strcmp("game_get_expected_nb_tents_row", argv[1]) == 0){
+    ok = test_game_get_expected_nb_tents_row();
+  }
+  else if(strcmp("game_get_expected_nb_tents_col", argv[1]) == 0){
+    ok = test_game_get_expected_nb_tents_col();
+  }
+  /*
+  else if(strcmp("game_get_expected_nb_tents_all", argv[1]) == 0){
+    ok = test_game_get_expected_nb_tents_all();
+  }
+  */
+  else {
+    fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
 
-  else if(strcmp("game_set_square", argv[1]) == 0){
-    ok = test_game_set_square();
-  }
-    */
-
-  else {
-    fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
-    exit(EXIT_FAILURE);
-  }
-
-  // print test result
-   if (ok) {
-    fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
-    return EXIT_SUCCESS;
-  } 
-  else {
-    fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
-    return EXIT_FAILURE;
-  }
-
+  // print test result
+   if (ok) {
+    fprintf(stderr, "Test \"%s\" finished: SUCCESS\n", argv[1]);
+    return EXIT_SUCCESS;
+  } 
+  else {
+    fprintf(stderr, "Test \"%s\" finished: FAILURE\n", argv[1]);
+    return EXIT_FAILURE;
+  }
 }
