cc879e8d719c1e0cdea5a49b8bc2957b56c5f534
Jennifer Ephrem <jennifer.ephrem@etu.u-bordeaux.fr>
2020-11-11 21:40:40 +0100

    modification game_equal

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -57,7 +57,7 @@ bool test_game_equal(void){
   }
   for(uint i = 0;i<DEFAULT_SIZE;i++){
     for(uint j=0;j<DEFAULT_SIZE;j++){
-      if(game_get_square(g1,i,j)!=game_get_square(g2,i,j) || game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g2,i) || game_get_expected_nb_tents_col(g1,j)!=game_get_expected_nb_tents_col(g2,j) || game_equal(g1,g2)==false){
+      if((game_get_square(g1,i,j)!=game_get_square(g2,i,j) || game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g2,i) || game_get_expected_nb_tents_col(g1,j)!=game_get_expected_nb_tents_col(g2,j)) && game_equal(g1,g2)==true){
         game_delete(g1);
         game_delete(g2);
         return false;
