78d65e5165a423faf117b48d0a730b7d357aa319
Jennifer Ephrem <jennifer.ephrem@etu.u-bordeaux.fr>
2020-11-11 20:57:43 +0100

    game_equal

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -47,14 +47,14 @@ bool test_game_delete(void){
 
 bool test_game_equal(void){
   game g1 = game_default();
-  game g2 = game_default();
+  game g2 = game_default_solution();
   if(g1 ==NULL || g2 == NULL){
     fprintf(stderr,"Error : at least one of the two pointers is invalid!\n");
     game_delete(g1);
     game_delete(g2);
     return false;
   }
-  if(game_equal(g1,g2)==false){
+  if(game_equal(g1,g2)==true){
     fprintf(stderr,"Error : the games are not equal!\n");
     game_delete(g1);
     game_delete(g2);
