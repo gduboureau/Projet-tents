6b5bbfbd3c402bb8a54406486c98604a99f24c9c
Almansa Hugo <hugo.almansa@etu.u-bordeaux.fr>
2020-11-11 21:45:57 +0100

    game check move hugo

--- game_test_valoustau.c
+++ game_test_valoustau.c
@@ -91,41 +91,53 @@ bool test_game_check_move(void){
             }*/
             //LOSING MOVE
             if (j!=0 && (game_get_square(g,i,j-1)==TENT && game_check_move(g,i,j,TENT)!=LOSING)){  //tent au dessus d'une autre
-                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOOSING!\n", i, j);
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
                 game_delete(g);
                 return false;
-                }
+            }
             if (j!=DEFAULT_SIZE-1 && (game_get_square(g,i,j+1)==TENT && game_check_move(g,i,j,TENT)!=LOSING)){ //tent en dessous d'une autre
-                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOOSING!\n", i, j);
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
                 game_delete(g);
                 return false;
-                }
+            }
             if (i!=0 && (game_get_square(g,i-1,j)==TENT && game_check_move(g,i,j,TENT)!=LOSING)){ //tent a gauche d'une autre
-                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOOSING!\n", i, j);
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
                 game_delete(g);
                 return false;
-                }
+            }
             if (i!=DEFAULT_SIZE-1 && (game_get_square(g,i+1,j)==TENT && game_check_move(g,i,j,TENT)!=LOSING)){ // tent a droite d'une autre
-                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOOSING!\n", i, j);
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
                 game_delete(g);
                 return false;
-                }
-                /*game g1 = game_default_solution();
-            if((game_get_current_nb_tents_row(g, i) >= game_get_current_nb_tents_row(g1, i)) && game_check_move(g, i, j, TENT) != LOSING && game_get_square(g,i,j)!=TREE){
-                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOOSING!\n", i, j);
+            }
+            
+            /*
+            if(game_get_expected_nb_tents_row(g, i)==0 && game_check_move(g, i, j, TENT) != LOSING){
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
                 game_delete(g);
-                game_delete(g1);
                 return false;
-                }
+            }
 
-            if((game_get_current_nb_tents_col(g, j) >= game_get_current_nb_tents_col(g1, j)) && game_check_move(g, i, j, TENT) != LOSING){
-                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOOSING!\n", i, j);
+            if(game_get_expected_nb_tents_col(g, j)==0 && game_check_move(g, i, j, TENT) != LOSING){
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
                 game_delete(g);
-                game_delete(g1);
                 return false;
-                }*/
+            }
+            */
+            
+            if(game_get_expected_nb_tents_row(g, i)>0 && (game_get_current_nb_tents_row(g, i) >= game_get_expected_nb_tents_row(g, i)) && game_check_move(g, i, j, TENT) != LOSING && game_get_square(g,i,j)!=TREE){
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
+                game_delete(g);
+                return false;
+            }
+
+            if(game_get_expected_nb_tents_col(g, j)>0 &&(game_get_current_nb_tents_col(g, j) >= game_get_expected_nb_tents_col(g, j)) && game_check_move(g, i, j, TENT) != LOSING && game_get_square(g,i,j)!=TREE){
+                fprintf(stderr,"Error: the game move at position (%d,%d) must be LOSING!\n", i, j);
+                game_delete(g);
+                return false;
             }
         }
+    }
     game_delete(g);
     return true;
 }
