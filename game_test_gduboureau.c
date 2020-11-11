1f049dd23794942bdc4b6797632168a1be2955e9
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2020-11-11 16:00:07 +0100

    ajout du cmakelists et game_test

--- game_test_gduboureau.c
+++ game_test_gduboureau.c
@@ -15,24 +15,57 @@ void usage (int argc, char *argv[]){
 
 bool test_game_fill_grass_row(){
     game g = game_default();
-    for (unsigned int i; i<DEFAULT_SIZE; i++){
-        for(unsigned int j = 0 ; j<DEFAULT_SIZE; j++){
-            if (game_check_move(g,i,j,TREE) == ILLEGAL){
+    game g1 = game_default();
+    for (unsigned int i = 0; i<DEFAULT_SIZE; i++){
+        game_fill_grass_row(g,i);
+    }
+    for (unsigned int i = 0; i<DEFAULT_SIZE; i++){
+        for (unsigned int j = 0; j<DEFAULT_SIZE; j++){
+            if ((game_get_square(g1,i,j) == EMPTY && game_get_square(g,i,j) != GRASS) || (game_get_square(g1,i,j) != EMPTY && game_get_square(g,i,j) == GRASS)){
+                game_delete(g);
+                game_delete(g1);
                 return false;
             }
         }
     }
+    game_delete(g);
+    game_delete(g1);
     return true;
 }
+
+
+/* *********************************************************** */
+
+bool test_game_fill_grass_col(){
+    game g = game_default();
+    game g1 = game_default();
+    for (unsigned int j = 0; j<DEFAULT_SIZE; j++){
+        game_fill_grass_col(g,j);
+    }
+    for (unsigned int i = 0; i<DEFAULT_SIZE; i++){
+        for (unsigned int j = 0; j<DEFAULT_SIZE; j++){
+            if ((game_get_square(g1,i,j) == EMPTY && game_get_square(g,i,j) != GRASS) || (game_get_square(g1,i,j) != EMPTY && game_get_square(g,i,j) == GRASS)){
+                game_delete(g);
+                game_delete(g1);
+                return false;
+            }
+        }
+    }
+    game_delete(g);
+    game_delete(g1);
+    return true;
+}
+
 
 /* *********************************************************** */
 
 bool test_game_restart(){
-    game g = game_default();
+    game g = game_default_solution();
     if (g==NULL){
         game_delete(g);
         return false;
     }
+    game_restart(g);
     for (unsigned int i = 0; i<DEFAULT_SIZE; i++){
         for (unsigned int j = 0; j<DEFAULT_SIZE; j++){
             if (game_get_square(g,i,j) == TENT || game_get_square(g,i,j) == GRASS){
@@ -41,8 +74,10 @@ bool test_game_restart(){
             }
         }
     }
-    if (game_is_over(g) == true){
+    game g1 = game_default();
+    if (game_equal(g,g1) == false){
         game_delete(g);
+        game_delete(g1);
         return false;
     }
     game_delete(g);
@@ -68,7 +103,14 @@ int main (int argc, char* argv[]){
     bool ok = false;
     if (strcmp("game_restart",argv[1]) == 0){
         ok = test_game_restart();
-    }else{
+    }
+    else if(strcmp("game_fill_grass_row", argv[1]) == 0){
+        ok = test_game_fill_grass_row();
+    }
+    else if(strcmp("game_fill_grass_col", argv[1]) == 0){
+        ok = test_game_fill_grass_col();
+    }
+    else{
         fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
         exit(EXIT_FAILURE);
     }
