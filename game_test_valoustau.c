f08d2067afd8f0b27bac4d24da1e8400f5c5579a
Valentin Loustau <valentin.loustau@etu.u-bordeaux.fr>
2020-11-11 16:03:07 +0100

    CMake + game_test : valoustau

--- game_test_valoustau.c
+++ game_test_valoustau.c
@@ -10,71 +10,103 @@ typedef unsigned int uint;
 /* ********** TEST GAME GET CURRENT NB TENTS ROW ********** */
 
 bool test__game_get_current_nb_tents_row(void){
-    cgame g = game_default();
+    game g = game_default();
     for (uint i=0; i<DEFAULT_SIZE; i++){
         if (game_get_current_nb_tents_row(g, i) > game_get_expected_nb_tents_row(g,i)){
             fprintf(stderr,"Error: current tents row is not expected (row %d)!\n", i);
+            game_delete(g);
             return false;
         }
     }
+    game_delete(g);
     return true;
 }
 
 /* ********** TEST GAME GET CURRENT NB TENTS COL ********** */
 
 bool test__game_get_current_nb_tents_col(void){
-    cgame g = game_default();
+    game g = game_default();
     for (uint j=0; j<DEFAULT_SIZE; j++){
         if (game_get_current_nb_tents_col(g, j) > game_get_expected_nb_tents_col(g,j)){
             fprintf(stderr,"Error: current tents col is not expected (col %d)!\n", j);
+            game_delete(g);
             return false;
         }
     }
+    game_delete(g);
     return true;
 }
 
 /* ********** TEST GAME GET CURRENT NB TENTS ALL ********** */
 
 bool test__game_get_current_nb_tents_all(void){
-    cgame g = game_default();
+    game g = game_default();
     uint c = 0;
     for (uint v=0; v<DEFAULT_SIZE; v++){
         c+=game_get_current_nb_tents_col(g, v);
     }
     if (game_get_current_nb_tents_all(g) != c){
         fprintf(stderr,"Error: number of all current tents is incorrect!\n");
+        game_delete(g);
         return false;
     }
+    game_delete(g);
     return true;
 }
 
 /* ********** GAME PLAY MOVE ********** */
 
 bool test__game_play_move(void){
-    cgame g = game_default();
+    game g = game_default();
     for (uint i=0; i<DEFAULT_SIZE; i++){
-        for(uint j=0; j<DEFAULT_SIZE; j++){
-            if (game_get_square (g,i,j)==TREE){
-                fprintf(stderr,"Error: the game square at position (%d,%d) must not be TREE!\n", i, j);
+        for (uint j=0; j<DEFAULT_SIZE; j++){
+            if (game_get_square(g,i,j)==TREE && ((game_play_move(g, i, j,TENT) || game_play_move(g, i, j,GRASS) || game_play_move(g, i, j,EMPTY)){
+                fprintf(stderr,"Error: illegal move!\n");
+                game_delete(g);
                 return false;
-            } 
+            }
         }
     }
+    game_delete(g);
     return true;
 }
 
 /* ********** GAME CHECK MOVE ********** */
 
 bool test__game_check_move(void){
-    cgame g = game_default();
+    game g = game_default();
     for (uint i=0; i<DEFAULT_SIZE; i++){
         for(uint j=0; j<DEFAULT_SIZE; j++){
             if (game_check_move(g, i, j, game_get_square (g,i,j))!=REGULAR){
                 fprintf(stderr,"Error: the game move at position (%d,%d) must be REGULAR!\n", i, j);
+                game_delete(g);
                 return false;
             } 
         }
     }
+    game_delete(g);
+    return true;
+}
+
+/* ********** GAME IS OVER ********** */
+
+bool test__game_is_over(void){
+    game g = game_default();
+    game g1 = game_default_solution();
+    if (game_is_over(g)==true && game_equal(g,g1)==false){
+        fprintf(stderr,"Error: the game over is different of game solution!\n");
+        game_delete(g);
+        game_delete(g1);
+        return false;
+    }
+    if (game_is_over(g)==false){
+        fprintf(stderr,"Error: the game over is different of game solution!\n");
+        game_delete(g);
+        game_delete(g1);
+        return false;
+    }
+    game_delete(g);
+    game_delete(g1);
     return true;
 }
 
@@ -106,6 +138,8 @@ int main(int argc, char *argv[]){
         ok = test__game_play_move();
     else if (strcmp("game_check_move", argv[1]) == 0)
         ok = test__game_check_move();
+    else if (strcmp("game_is_over", argv[1]) == 0)
+        ok = test__game_is_over();
     else {
     fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
     exit(EXIT_FAILURE);
