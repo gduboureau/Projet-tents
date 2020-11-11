32dafdf6de4d520cc0c6c50324368fb0c00b1cee
Jennifer Ephrem <jennifer.ephrem@etu.u-bordeaux.fr>
2020-11-11 22:23:13 +0100

    modification game_equal

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -48,25 +48,25 @@ bool test_game_delete(void){
 bool test_game_equal(void){
   game g1 = game_default();
   game g2 = game_default();
+  game g3 = game_default_solution();
+  game g4 = game_new_empty();
 
-  if(g1 ==NULL || g2 == NULL){
-    fprintf(stderr,"Error : at least one of the two pointers is invalid!\n");
-    game_delete(g1);
-    game_delete(g2);
-    return false;
-  }
   for(uint i = 0;i<DEFAULT_SIZE;i++){
     for(uint j=0;j<DEFAULT_SIZE;j++){
-      if((game_get_square(g1,i,j)!=game_get_square(g2,i,j) || game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g2,i) || game_get_expected_nb_tents_col(g1,j)!=game_get_expected_nb_tents_col(g2,j)) && game_equal(g1,g2)==true){
+      if(game_get_square(g1,i,j)!=game_get_square(g2,i,j) || game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g2,i) || game_get_expected_nb_tents_col(g1,j)!=game_get_expected_nb_tents_col(g2,j)){
         game_delete(g1);
         game_delete(g2);
+        game_delete(g3);
         return false;
       }
+      if(game_get_square(g1,i,j)!=game_get_square(g3,i,j) || game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g3,i) || game_get_expected_nb_tents_col(g1,j)!=game_get_expected_nb_tents_col(g3,j))
+
     }
   }
   
   game_delete(g1);
   game_delete(g2);
+  game_delete(g3);
   return true;
 }
 
