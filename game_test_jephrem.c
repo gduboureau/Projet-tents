3081a5a1a7494d5d03fad683a3386d5742b8a763
Jennifer Ephrem <jennifer.ephrem@etu.u-bordeaux.fr>
2020-11-12 16:30:26 +0100

    modification game_equal

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -60,15 +60,7 @@ bool test_game_equal(void){
         game_delete(g3);
         game_delete(g4);
         return false;
-      }
-      if(game_get_square(g1,i,j)==game_get_square(g3,i,j) && game_get_expected_nb_tents_row(g1,i)!= game_get_expected_nb_tents_row(g3,i) && game_get_expected_nb_tents_col(g1,j)== game_get_expected_nb_tents_col(g3,j) && game_get_current_nb_tents_all(g1)== game_get_current_nb_tents_all(g3)){
-        fprintf(stderr,"the games are not equal!\n");
-        game_delete(g1);
-        game_delete(g2);
-        game_delete(g3);
-        game_delete(g4);
-        return false;
-      }      
+      } 
     }
   } 
   game_delete(g1);
