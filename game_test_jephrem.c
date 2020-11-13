aa03c77a9c85c03ff5e66cb02eebad8f87486352
Almansa Hugo <hugo.almansa@etu.u-bordeaux.fr>
2020-11-13 17:06:07 +0100

    game equal jennifer6

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -48,11 +48,17 @@ bool test_game_equal(void){
   
   for(uint i = 0;i<DEFAULT_SIZE;i++){
     for(uint j = 0;j<DEFAULT_SIZE;j++){
-      if(game_get_square(g1, i, j)==game_get_square(g2, i, j)){
+      if(game_get_square(g1, i, j)==game_get_square(g2, i, j) && game_get_expected_nb_tents_row(g1, i)==game_get_expected_nb_tents_row(g2, i) && game_get_expected_nb_tents_col(g1, j)==game_get_expected_nb_tents_col(g2, j)){
         c++;
       }
       if(game_get_square(g1, i, j)==game_get_square(g3, i, j)){
         d++;
+      }
+      if(game_equal(g1,g3)==true && (game_get_expected_nb_tents_row(g1, i)!=game_get_expected_nb_tents_row(g3, i) || game_get_expected_nb_tents_col(g1, j)!=game_get_expected_nb_tents_col(g3, j))){
+        game_delete(g1);
+        game_delete(g2);
+        game_delete(g3); 
+        return false;
       }
     }
   }
