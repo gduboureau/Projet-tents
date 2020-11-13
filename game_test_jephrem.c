b4de4cf5168c62fdf7d8fcd2e944214a6dd8cc4e
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2020-11-13 21:25:57 +0100

    test game equal 3

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -54,7 +54,7 @@ bool test_game_equal(void){
         game_delete(g4);
         return false;
       }
-      if (((game_get_square(g1,i,j) == game_get_square(g2,i,j)) && game_get_expected_nb_tents_col(g1,j)==game_get_expected_nb_tents_col(g2,j) && game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g2,i)) && game_equal(g3,g4) == false){
+      if ((game_get_square(g1,i,j) == game_get_square(g2,i,j) && game_get_expected_nb_tents_col(g1,j)==game_get_expected_nb_tents_col(g2,j) && game_get_expected_nb_tents_row(g1,i)==game_get_expected_nb_tents_row(g2,i)) && game_equal(g1,g2) == false){
         game_delete(g1);
         game_delete(g2);
         game_delete(g3);
