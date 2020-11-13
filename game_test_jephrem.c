3388ac8ebf4700ec050259d6bdb2a73bf113c3db
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2020-11-13 21:35:07 +0100

    test game equal 4

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -42,31 +42,35 @@ bool test_game_delete(void){
 bool test_game_equal(void){
   game g1 = game_default();
   game g2 = game_default();
-  game g3 = game_default_solution();
-  game g4 = game_new_empty();
-  
-  for(uint i = 0;i<DEFAULT_SIZE;i++){
-    for(uint j = 0;j<DEFAULT_SIZE;j++){
-      if ((game_get_square(g1,i,j) != game_get_square(g2,i,j) || game_get_expected_nb_tents_col(g1,j)!=game_get_expected_nb_tents_col(g2,j) || game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g2,i)) && game_equal(g1,g2) == true){
-        game_delete(g1);
-        game_delete(g2);
-        game_delete(g3);
-        game_delete(g4);
-        return false;
-      }
-      if ((game_get_square(g1,i,j) == game_get_square(g2,i,j) && game_get_expected_nb_tents_col(g1,j)==game_get_expected_nb_tents_col(g2,j) && game_get_expected_nb_tents_row(g1,i)==game_get_expected_nb_tents_row(g2,i)) && game_equal(g1,g2) == false){
-        game_delete(g1);
-        game_delete(g2);
-        game_delete(g3);
-        game_delete(g4);
-        return false;
+
+  if(game_equal(g1,g2) == true){
+    for(uint i = 0;i<DEFAULT_SIZE;i++){
+      for(uint j = 0;j<DEFAULT_SIZE;j++){
+        if (game_get_square(g1,i,j) != game_get_square(g2,i,j) || game_get_expected_nb_tents_col(g1,j)!=game_get_expected_nb_tents_col(g2,j) || game_get_expected_nb_tents_row(g1,i)!=game_get_expected_nb_tents_row(g2,i)){
+          game_delete(g1);
+          game_delete(g2);
+          return false;
+        }
       }
     }
   }
+  int a = 0;
+  if(game_equal(g1,g2) == false){
+    for(uint i = 0;i<DEFAULT_SIZE;i++){
+      for(uint j = 0;j<DEFAULT_SIZE;j++){
+        if (game_get_square(g1,i,j) == game_get_square(g2,i,j) && game_get_expected_nb_tents_col(g1,j)==game_get_expected_nb_tents_col(g2,j) && game_get_expected_nb_tents_row(g1,i)==game_get_expected_nb_tents_row(g2,i)){
+          a++;
+        }
+      }
+    }
+  }
+  if (a>60){
+    game_delete(g1);
+    game_delete(g2);
+    return false;
+  }
   game_delete(g1);
   game_delete(g2);
-  game_delete(g3);
-  game_delete(g4);
   return true;
 }
 
