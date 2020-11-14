a9e05fe0b22d00c2219928da14a9158bc3266a53
Almansa Hugo <hugo.almansa@etu.u-bordeaux.fr>
2020-11-14 16:36:51 +0100

    game equal v22

--- game_test_jephrem.c
+++ game_test_jephrem.c
@@ -45,7 +45,7 @@ bool test_game_equal(void){
   uint c = 0;
   for(int i = 0; i <DEFAULT_SIZE; i++){
      for(int j = 0; j <DEFAULT_SIZE; j++){
-       if(game_get_square(g1, i, j) != game_get_square(g2, i, j) || game_get_expected_nb_tents_row(g1, i) != game_get_expected_nb_tents_row(g2, i) || game_get_expected_nb_tents_col(g1, j) != game_get_expected_nb_tents_col(g2, j)){
+       if(game_equal(g1, g2)==true && (game_get_square(g1, i, j) != game_get_square(g2, i, j) || game_get_expected_nb_tents_row(g1, i) != game_get_expected_nb_tents_row(g2, i) || game_get_expected_nb_tents_col(g1, j) != game_get_expected_nb_tents_col(g2, j))){
          game_delete(g1);
          game_delete(g2);
          return false;
@@ -68,7 +68,7 @@ game_set_expected_nb_tents_row(g1, 0, 1);
     game_delete(g2);
     return false;
   }
-  
+
   game_delete(g1);
   game_delete(g2);
     return true;
