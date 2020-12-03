0a620953fdf028e4820add4fb420fbe995366bc1
Almansa Hugo <hugo.almansa@etu.u-bordeaux.fr>
2020-12-03 11:48:10 +0100

    game.c1

--- game.c
+++ game.c
@@ -17,6 +17,8 @@ typedef struct game_s *game;
 
 typedef const struct game_s *cgame;
 
+/********************* Jennifer *********************/
+
 game game_new(square *squares, uint *nb_tents_row, uint *nb_tents_col) {
   return 0;
 }
@@ -39,13 +41,15 @@ game game_new_empty(void) {
   }
   return 0; }
 
-game game_copy(cgame g) { return 1; }
+game game_copy(cgame g) { return 0; }
 
-bool game_equal(cgame g1, cgame g2) { return 1; }
+bool game_equal(cgame g1, cgame g2) { return 0; }
 
 void game_delete(game g) { return; }
 
 void game_set_square(game g, uint i, uint j, square s) { return; }
+
+/********************* Hugo *********************/
 
 square game_get_square(cgame g, uint i, uint j) { return 0; }
 
@@ -57,6 +61,8 @@ uint game_get_expected_nb_tents_row(cgame g, uint i) { return 0; }
 
 uint game_get_expected_nb_tents_col(cgame g, uint j) { return 0; }
 
+/********************* Valentin *********************/
+
 uint game_get_expected_nb_tents_all(cgame g) { return 0; }
 
 uint game_get_current_nb_tents_row(cgame g, uint i) { return 0; }
@@ -66,6 +72,8 @@ uint game_get_current_nb_tents_col(cgame g, uint j) { return 0; }
 uint game_get_current_nb_tents_all(cgame g) { return 0; }
 
 void game_play_move(game g, uint i, uint j, square s) { return; }
+
+/********************* Guillaume *********************/
 
 int game_check_move(cgame g, uint i, uint j, square s) { return 0; }
 
