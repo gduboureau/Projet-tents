173573ccdb39d2ee34af8b7c5fc7dfc490de943d
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2020-12-07 15:48:20 +0100

    version non beuge enfin (j'espère)

--- game.c
+++ game.c
@@ -379,7 +379,10 @@ int game_check_move(cgame g, uint i, uint j, square s) {
   }
 
   /* Analyse si tente non attendue dans une row/col */
-
+  if (s == TENT && (game_get_expected_nb_tents_col(g, j) == 0 ||
+                    game_get_expected_nb_tents_row(g, i) == 0)) {
+    return LOSING;
+  }
   if (s == TENT && ((game_get_expected_nb_tents_row(g, i) <
                      game_get_current_nb_tents_row(g, i)) ||
                     (game_get_expected_nb_tents_col(g, j) <
