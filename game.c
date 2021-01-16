7da13272415462bcc206e2f26d3b41fd67a4226a
Almansa Hugo <hugo.almansa@etu.u-bordeaux.fr>
2021-01-16 14:51:53 +0100

    check move bug1

--- game.c
+++ game.c
@@ -552,7 +552,7 @@ static bool r3_tent_next_to_tree(cgame g, uint x, uint y, square s) {
   if (s == TENT) {
     for (int i = -1; i < 2; i++) {
       for (int j = -1; j < 2; j++) {
-        if ((i == 0 || j == 0) &&
+        if ((i == 0 || j == 0) && (i + j != 0) &&
             (correct_next_coor(g, make_coor(x, y),
                                coor_to_dir(make_coor(i, j))))) {
           if (game_get_square(g, x + i, y + j) != TREE) {
