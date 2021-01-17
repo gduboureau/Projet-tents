396418690d5f143bcef38cbbf0ce6e9ee9108c3b
Almansa Hugo <hugo.almansa@etu.u-bordeaux.fr>
2021-01-17 17:36:49 +0100

    test pour connaitre le test 1 v2

--- game.c
+++ game.c
@@ -1298,13 +1298,13 @@ int game_check_move(cgame g, uint i, uint j, square s) {
    * pour placer les tentes attendues */
 
   /*Compteur de EMPTY colonne*/
-  uint c = 0;
+  /*uint c = 0;
   for (uint x = 0; x < DEFAULT_SIZE; x++) {
     if (game_get_square(g, x, j) == EMPTY) {
       c++;
     }
   }
-  /*Compteur de EMPTY ligne*/
+  Compteur de EMPTY ligne
   uint d = 0;
   for (uint y = 0; y < DEFAULT_SIZE; y++) {
     if (game_get_square(g, i, y) == EMPTY) {
@@ -1318,6 +1318,7 @@ int game_check_move(cgame g, uint i, uint j, square s) {
                             game_get_current_nb_tents_col(g, j))))) {
     return LOSING;
   }
+  */
   return REGULAR;
 }
 
