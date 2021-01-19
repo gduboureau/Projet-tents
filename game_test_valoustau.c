80c899f02a3649eb30e96e7fe754e1af53332e56
Valentin Loustau <valentin.loustau@etu.u-bordeaux.fr>
2021-01-19 12:22:43 +0100

    test_valoustau check move (rules)

--- game_test_valoustau.c
+++ game_test_valoustau.c
@@ -27,7 +27,6 @@ square squares2[] = {
     TREE,  GRASS, TREE,  GRASS, TENT,  GRASS, GRASS, GRASS, GRASS, GRASS, GRASS,
     GRASS, TREE,  GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS};
 
-
 /* ********** TEST GAME GET CURRENT NB TENTS ROW ********** */
 
 bool test_game_get_current_nb_tents_row(void) {
@@ -119,23 +118,25 @@ bool test_game_play_move(void) {
 /* ********** GAME CHECK MOVE ********** */
 
 bool test_game_check_move(void) {
+  // DIAGADJ
   game g = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, true);
-  game_set_square(g,0,0,TENT);
-  if (game_check_move(g,1,1,TENT) == LOSING){
+  game_set_square(g, 0, 0, TENT);
+  if (game_check_move(g, 1, 1, TENT) == LOSING) {
     game_delete(g);
     return false;
   }
   game g1 = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, false);
-  game_set_square(g1,0,0,TENT);
-  if (game_check_move(g1,1,1,TENT) != LOSING){
+  game_set_square(g1, 0, 0, TENT);
+  if (game_check_move(g1, 1, 1, TENT) != LOSING) {
     game_delete(g);
     game_delete(g1);
     return false;
   }
+  // WRAPPING
   game g2 = game_new_ext(8, 8, squares, tentes_lig, tentes_col, true, true);
   game g3 = game_new_ext(8, 8, squares, tentes_lig, tentes_col, true, false);
 
-  if(game_check_move(g3,7,7,TENT) == LOSING){
+  if (game_check_move(g3, 7, 7, TENT) == LOSING) {
     game_delete(g);
     game_delete(g1);
     game_delete(g2);
@@ -172,9 +173,55 @@ bool test_game_check_move(void) {
   game_delete(g1);
   game_delete(g2);
   game_delete(g3);
+
+  // CHECK_MOVE
+  // r2_nb_tent_respecte
+  game g4 = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, false);
+  game_set_square(g4, 6, 7, TENT);
+  if (game_check_move(g4, 7, 7, TENT) != LOSING) {
+    game_delete(g4);
+    return false;
+  }
+
+  // r3_tent_next_to_tree
+  game g5 = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, false);
+  if (game_check_move(g5, 4, 6, TENT) == LOSING) {
+    game_delete(g4);
+    game_delete(g5);
+    return false;
+  }
+  if (game_check_move(g5, 6, 2, TENT) != LOSING) {
+    game_delete(g4);
+    game_delete(g5);
+    return false;
+  }
+
+  // r4_nb_tent_grass
+  game g6 = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, false);
+  if (game_check_move(g6, 0, 0, GRASS) != LOSING) {
+    game_delete(g4);
+    game_delete(g5);
+    game_delete(g6);
+    return false;
+  }
+
+  // r5_tree_entoure_grass
+  game g7 = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, false);
+  game_set_square(g7, 6, 0, GRASS);
+  if (game_check_move(g7, 7, 1, GRASS) != LOSING) {
+    game_delete(g4);
+    game_delete(g5);
+    game_delete(g6);
+    game_delete(g7);
+    return false;
+  }
+
+  game_delete(g4);
+  game_delete(g5);
+  game_delete(g6);
+  game_delete(g7);
   return true;
 }
-
 
 /* ********** USAGE ********** */
 
