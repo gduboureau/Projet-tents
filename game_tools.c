adf9dd94968f51a050abb749e9154089842154b0
Jennifer Ephrem <jennifer.ephrem@etu.u-bordeaux.fr>
2021-02-06 11:53:06 +0100

    fonction game_save

--- game_tools.c
+++ game_tools.c
@@ -14,30 +14,29 @@ void game_save(cgame g, char *filename) {
   if (f == NULL) {
     exit(EXIT_FAILURE);
   }
-  fprintf(f, "%u %u %u %u \n", game_nb_rows(g), game_nb_cols(g),
+  fprintf(f, "%u %u %u %u\n", game_nb_rows(g), game_nb_cols(g),
           game_is_wrapping(g), game_is_diagadj(g));
   for (uint i = 0; i < game_nb_rows(g); i++) {
     fprintf(f, "%u ", game_get_expected_nb_tents_row(g, i));
     fprintf(f, "\n");
   }
-  for (uint i = 0; i < game_nb_rows(g); i++) {
-    fprintf(f, "%u ", game_get_expected_nb_tents_row(g, i));
+  for (uint i = 0; i < game_nb_cols(g); i++) {
+    fprintf(f, "%u ", game_get_expected_nb_tents_col(g, i));
     fprintf(f, "\n");
   }
-  unsigned int j;
   for (unsigned int i = 0; i < game_nb_rows(g); i++) {
-    for (j = 0; j < game_nb_cols(g); j++) {
+    for (unsigned int j = 0; j < game_nb_cols(g); j++) {
       if (game_get_square(g, i, j) == EMPTY) {
-        fprintf(f, "")  ;
+        fprintf(f, " ");
       }
       if (game_get_square(g, i, j) == TREE) {
-        fprintf(f, "x ");
+        fprintf(f, "x");
       }
       if (game_get_square(g, i, j) == TENT) {
-        fprintf(f, "* ");
+        fprintf(f, "*");
       }
       if (game_get_square(g, i, j) == GRASS) {
-        fprintf(f, "- ");
+        fprintf(f, "-");
       }
     }
   }
