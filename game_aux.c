e33ff0194007de5d0b28845e05c7826741909a94
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2021-02-25 16:17:49 +0100

    nos fichiers

--- game_aux.c
+++ game_aux.c
@@ -1,82 +1,87 @@
-/**
- * @file game_aux.c
- * @copyright University of Bordeaux. All rights reserved, 2020.
- *
- **/
-
 #include "game_aux.h"
-#include "game.h"
-#include "game_ext.h"
-#include "game_private.h"
-
-#include <assert.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/* ************************************************************************** */
+#include "game.h"
+#include "game_ext.h"
 
 void game_print(cgame g) {
-  assert(g);
   printf("   ");
-  for (uint j = 0; j < game_nb_cols(g); j++) printf("%d", j);
-  printf("\n");
-  printf("   ");
-  for (uint j = 0; j < game_nb_cols(g); j++) printf("-");
-  printf("\n");
-  for (uint i = 0; i < game_nb_rows(g); i++) {
-    printf("%d |", i);
-    for (uint j = 0; j < game_nb_cols(g); j++) {
-      square s = game_get_square(g, i, j);
-      char c = _square2str(s);
-      printf("%c", c);
-    }
-    printf("| %d\n", game_get_expected_nb_tents_row(g, i));
+  for (unsigned int y = 0; y < game_nb_cols(g); y++) {
+    printf("%d", y);
   }
-  printf("   ");
-  for (uint j = 0; j < game_nb_cols(g); j++) printf("-");
-  printf("\n");
-  printf("   ");
-  for (uint j = 0; j < game_nb_cols(g); j++) printf("%d", game_get_expected_nb_tents_col(g, j));
-  printf("\n");
-}
+  printf("\n   ");
+  for (unsigned int z = 0; z < game_nb_cols(g); z++) {
+    printf("-");
+  }
+  unsigned int j;
+  for (unsigned int i = 0; i < game_nb_rows(g); i++) {
+    printf("\n%d |", i);
+    for (j = 0; j < game_nb_cols(g); j++) {
+      if (game_get_square(g, i, j) == EMPTY) {
+        printf(" ");
+      }
+      if (game_get_square(g, i, j) == TREE) {
+        printf("x");
+      }
+      if (game_get_square(g, i, j) == TENT) {
+        printf("*");
+      }
+      if (game_get_square(g, i, j) == GRASS) {
+        printf("-");
+      }
+    }
+    if (j == game_nb_cols(g)) {
+      uint lign = game_get_expected_nb_tents_row(g, i);
+      printf("| %u", lign);
+    }
+  }
 
-/* ************************************************************************** */
+  printf("\n   ");
+
+  for (unsigned int z = 0; z < game_nb_cols(g); z++) {
+    printf("-");
+  }
+  printf("\n   ");
+  for (unsigned int w = 0; w < game_nb_cols(g); w++) {
+    uint colo = game_get_expected_nb_tents_col(g, w);
+    printf("%u", colo);
+  }
+  printf("\n");
+
+  return;
+}
 
 game game_default(void) {
-  square squares[] = {
-      EMPTY, EMPTY, EMPTY, EMPTY, TREE,  TREE,  EMPTY, EMPTY, /* row 0 */
-      TREE,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, TREE,  /* row 1 */
-      EMPTY, EMPTY, EMPTY, EMPTY, TREE,  EMPTY, EMPTY, EMPTY, /* row 2 */
-      TREE,  EMPTY, EMPTY, EMPTY, EMPTY, TREE,  EMPTY, EMPTY, /* row 3 */
-      EMPTY, TREE,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, /* row 4 */
-      TREE,  EMPTY, EMPTY, EMPTY, TREE,  EMPTY, TREE,  EMPTY, /* row 5 */
-      EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, /* row 6 */
-      TREE,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, /* row 7 */
-  };
-  uint nb_tents_row[] = {3, 0, 4, 0, 4, 0, 1, 0};
-  uint nb_tents_col[] = {4, 0, 1, 2, 1, 1, 2, 1};
-  return game_new(squares, nb_tents_row, nb_tents_col);
-}
+  uint tentes_lig[] = {3, 0, 4, 0, 4, 0, 1, 0};
+  uint tentes_col[] = {4, 0, 1, 2, 1, 1, 2, 1};
 
-/* ************************************************************************** */
+  square squares[] = {EMPTY, EMPTY, EMPTY, EMPTY, TREE,  TREE,  EMPTY, EMPTY,
+                      TREE,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, TREE,
+                      EMPTY, EMPTY, EMPTY, EMPTY, TREE,  EMPTY, EMPTY, EMPTY,
+                      TREE,  EMPTY, EMPTY, EMPTY, EMPTY, TREE,  EMPTY, EMPTY,
+                      EMPTY, TREE,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                      TREE,  EMPTY, EMPTY, EMPTY, TREE,  EMPTY, TREE,  EMPTY,
+                      EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                      TREE,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY};
 
-game game_default_solution(void) {
-  game g = game_default();
-  game_play_move(g, 0, 0, TENT);
-  game_play_move(g, 0, 3, TENT);
-  game_play_move(g, 0, 6, TENT);
-  game_play_move(g, 2, 0, TENT);
-  game_play_move(g, 2, 3, TENT);
-  game_play_move(g, 2, 5, TENT);
-  game_play_move(g, 2, 7, TENT);
-  game_play_move(g, 4, 0, TENT);
-  game_play_move(g, 4, 2, TENT);
-  game_play_move(g, 4, 4, TENT);
-  game_play_move(g, 4, 6, TENT);
-  game_play_move(g, 6, 0, TENT);
-  for (uint i = 0; i < DEFAULT_SIZE; i++) game_fill_grass_row(g, i);
+  game g = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, false);
+
   return g;
 }
 
-/* ************************************************************************** */
+game game_default_solution(void) {
+  uint tentes_lig[] = {3, 0, 4, 0, 4, 0, 1, 0};
+  uint tentes_col[] = {4, 0, 1, 2, 1, 1, 2, 1};
+
+  square squares[] = {TENT, GRASS, GRASS, TENT,  TREE,  TREE,  TENT,  GRASS,
+                      TREE, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, TREE,
+                      TENT, GRASS, GRASS, TENT,  TREE,  TENT,  GRASS, TENT,
+                      TREE, GRASS, GRASS, GRASS, GRASS, TREE,  GRASS, GRASS,
+                      TENT, TREE,  TENT,  GRASS, TENT,  GRASS, TENT,  GRASS,
+                      TREE, GRASS, GRASS, GRASS, TREE,  GRASS, TREE,  GRASS,
+                      TENT, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS,
+                      TREE, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS};
+  game g = game_new_ext(8, 8, squares, tentes_lig, tentes_col, false, false);
+
+  return g;
+}
