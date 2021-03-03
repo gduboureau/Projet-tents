3bce87650af9cfb3c801db44102282314200f00b
Guillaume Duboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2021-03-03 11:19:30 +0100

    fichier game_solve.c non fini

--- game_solve.c
+++ game_solve.c
@@ -8,11 +8,27 @@
 #include "game_tools.h"
 
 int main(int argc, char *argv[]) {
-    game g = game_load(argv[1]);
-    if (game_solve(g)){
-        game_print(g);
+    if (argc < 3 || argc > 4){
+        fprintf(stderr,"wrong number of parameters\n");
+        exit(EXIT_FAILURE);
+    }
+    game g = game_load(argv[2]);    
+    if( strcmp(argv[1], "-s") == 0) {
+        if (game_solve(g)){
+            if (argc == 4){
+                game_save(g,argv[3]);
+                printf("Solution dans le fichier de sortie\n");
+            }else{
+                game_print(g);
+            }
+        }else{
+            fprintf(stderr,"pas de solution\n");
+            exit(EXIT_FAILURE);
+        }
+    }else if( strcmp(argv[1], "-c") == 0) {
+        printf("n'existe pas encore\n");
     }else{
-        fprintf(stderr,"pas de solution");
+        fprintf(stderr,"please enter s for solve the game or c to find the number of solution");
         exit(EXIT_FAILURE);
     }
     return EXIT_SUCCESS;
